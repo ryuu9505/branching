f6852b9bbe834767f0aef40da7f77a843638514d
siyoon <ryuu.public@gmail.com>
2024-07-25 23:12:38 +0900

    first commit

---
+++ src/main/java/com/xiyoon/branching/BranchingApplication.java
@@ -0,0 +1,13 @@
+package com.xiyoon.branching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiyoon/branching/BranchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiyoon.branching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
